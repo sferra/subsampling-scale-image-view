a890c2f6e7ce210838d27400a9f670920b78b884
Cristian Beskid <schizosfera@gmail.com>
2015-03-05 15:21:58 +0100

    Deduplicate constants and move them out of the way (in the DeprecatedConstants interface) so we cann proceed more easily with the refactoring

--- library/src/com/davemorrissey/labs/subscaleview/ScaleImageView.java
+++ library/src/com/davemorrissey/labs/subscaleview/ScaleImageView.java
@@ -47,8 +47,6 @@ import com.davemorrissey.labs.subscaleview.decoder.ImageDecoder;
 import com.davemorrissey.labs.subscaleview.decoder.SkiaImageDecoder;
 
 import java.lang.ref.WeakReference;
-import java.util.Arrays;
-import java.util.List;
 
 /**
  * An alternative to {@link SubsamplingScaleImageView}, this reproduces all the same event handling, animation and
@@ -64,40 +62,6 @@ import java.util.List;
 public class ScaleImageView extends ScaleImageViewBase {
 
     private static final String TAG = ScaleImageView.class.getSimpleName();
-
-    /** During zoom animation, keep the point of the image that was tapped in the same place, and scale the image around it. */
-    public static final int ZOOM_FOCUS_FIXED = 1;
-    /** During zoom animation, move the point of the image that was tapped to the center of the screen. */
-    public static final int ZOOM_FOCUS_CENTER = 2;
-    /** Zoom in to and center the tapped point immediately without animating. */
-    public static final int ZOOM_FOCUS_CENTER_IMMEDIATE = 3;
-
-    private static final List<Integer> VALID_ZOOM_STYLES = Arrays.asList(ZOOM_FOCUS_FIXED, ZOOM_FOCUS_CENTER, ZOOM_FOCUS_CENTER_IMMEDIATE);
-
-    /** Quadratic ease out. Not recommended for scale animation, but good for panning. */
-    public static final int EASE_OUT_QUAD = 1;
-    /** Quadratic ease in and out. */
-    public static final int EASE_IN_OUT_QUAD = 2;
-
-    private static final List<Integer> VALID_EASING_STYLES = Arrays.asList(EASE_IN_OUT_QUAD, EASE_OUT_QUAD);
-
-    /** Don't allow the image to be panned off screen. As much of the image as possible is always displayed, centered in the view when it is smaller. This is the best option for galleries. */
-    public static final int PAN_LIMIT_INSIDE = 1;
-    /** Allows the image to be panned until it is just off screen, but no further. The edge of the image will stop when it is flush with the screen edge. */
-    public static final int PAN_LIMIT_OUTSIDE = 2;
-    /** Allows the image to be panned until a corner reaches the center of the screen but no further. Useful when you want to pan any spot on the image to the exact center of the screen. */
-    public static final int PAN_LIMIT_CENTER = 3;
-
-    private static final List<Integer> VALID_PAN_LIMITS = Arrays.asList(PAN_LIMIT_INSIDE, PAN_LIMIT_OUTSIDE, PAN_LIMIT_CENTER);
-
-    /** Scale the image so that both dimensions of the image will be equal to or less than the corresponding dimension of the view. The image is then centered in the view. This is the default behaviour and best for galleries. */
-    public static final int SCALE_TYPE_CENTER_INSIDE = 1;
-    /** Scale the image uniformly so that both dimensions of the image will be equal to or larger than the corresponding dimension of the view. The image is then centered in the view. */
-    public static final int SCALE_TYPE_CENTER_CROP = 2;
-    /** Scale the image so that both dimensions of the image will be equal to or less than the maxScale and equal to or larger than minScale. The image is then centered in the view. */
-    public static final int SCALE_TYPE_CUSTOM = 3;
-
-    private static final List<Integer> VALID_SCALE_TYPES = Arrays.asList(SCALE_TYPE_CENTER_CROP, SCALE_TYPE_CENTER_INSIDE, SCALE_TYPE_CUSTOM);
 
     // The bitmap to be displayed
     private Bitmap bitmap;
